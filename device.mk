61b62f24d16518339e9cad2f0eda3b2ee3ac0ec4
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    tenet: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- device.mk
+++ device.mk
@@ -302,8 +302,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor:64 \
     android.hardware.secure_element@1.2.vendor:64 \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 # OMX
