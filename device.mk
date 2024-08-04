3b568fc3385244e9e0790790d1990431209da368
Alexander Koskovich <zvnexus@outlook.com>
2024-08-04 16:42:07 +0700

    tenet: Remove vendor RenderScript implementation.
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: I19460ef266a646b046f1e7d2f0b4eab7c48ae536

--- device.mk
+++ device.mk
@@ -175,7 +175,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.renderscript@1.0-impl \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
     libqdMetaData \
