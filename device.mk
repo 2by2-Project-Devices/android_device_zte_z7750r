7ed1699c4263bffc504f7c6f4832df08731cc225
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    tenet: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- device.mk
+++ device.mk
@@ -336,7 +336,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 # vndservicemanager
 PRODUCT_PACKAGES += \
