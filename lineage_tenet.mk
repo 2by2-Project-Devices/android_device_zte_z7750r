c7fd1c9615f38c945f84a1b4f087f3956cfc9c2c
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    tenet: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_tenet.mk
+++ lineage_tenet.mk
@@ -24,9 +24,7 @@ PRODUCT_NAME := lineage_tenet
 PRODUCT_GMS_CLIENTID_BASE := android-zte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="CN_P768A02-user 11 RKQ1.210303.002 20231225.163043 release-keys" \
-    TARGET_DEVICE="P768A02" \
-    TARGET_PRODUCT="CN_P768A02"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "ZTE/CN_P768A02/P768A02:11/RKQ1.210303.002/20231225.163043:user/release-keys"
+    BuildDesc="CN_P768A02-user 11 RKQ1.210303.002 20231225.163043 release-keys" \
+    BuildFingerprint=ZTE/CN_P768A02/P768A02:11/RKQ1.210303.002/20231225.163043:user/release-keys \
+    DeviceName=P768A02 \
+    DeviceProduct=CN_P768A02
