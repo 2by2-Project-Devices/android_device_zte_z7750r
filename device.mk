94582daee7afc315cdf710c62aa24a3dfce71ba3
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    tenet: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -242,6 +242,11 @@ PRODUCT_PACKAGES += \
     Tag
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
