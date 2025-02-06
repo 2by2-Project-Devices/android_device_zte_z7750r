84c32c4d80e77629b04a6ff6276b92602646b237
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    tenet: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2021-2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -267,7 +267,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
